2645ba37d9146d0198a2f6abf2e4d2e09d950e71
huangweihua <744302215@qq.com>
2023-02-10 17:07:05 +0800

    增加拼接租户异常日志

--- solomon-cache/src/main/java/com/steven/solomon/service/AbsICacheService.java
+++ solomon-cache/src/main/java/com/steven/solomon/service/AbsICacheService.java
@@ -2,12 +2,16 @@ package com.steven.solomon.service;
 
 import com.steven.solomon.enums.CacheModeEnum;
 import com.steven.solomon.holder.HeardHolder;
+import com.steven.solomon.logger.LoggerUtils;
 import com.steven.solomon.profile.CacheProfile;
 import com.steven.solomon.verification.ValidateUtils;
 
 import javax.annotation.Resource;
+import org.slf4j.Logger;
 
 public abstract class AbsICacheService implements  ICacheService {
+
+  private Logger logger = LoggerUtils.logger(AbsICacheService.class);
 
   @Resource
   private CacheProfile cacheProfile;
@@ -16,7 +20,9 @@ public abstract class AbsICacheService implements  ICacheService {
     StringBuilder sb = new StringBuilder();
     if(ValidateUtils.isNotEmpty(cacheProfile) && CacheModeEnum.TENANT_PREFIX.toString().equals(cacheProfile.getMode())){
       String tenantId = HeardHolder.getTenantCode();
-      if(ValidateUtils.isNotEmpty(tenantId)){
+      if(ValidateUtils.isEmpty(tenantId)){
+        logger.error("当前模式是:{},但是缺乏租户信息,所以不拼接",CacheModeEnum.TENANT_PREFIX.getDesc());
+      } else {
         sb.append(tenantId).append(":");
       }
     }
