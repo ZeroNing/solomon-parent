f0c322b54ffe2e36555c415637a49fe3c15d3028
huangweihua <744302215@qq.com>
2023-02-15 11:24:10 +0800

    提交部分mybatis多数据源代码

---
+++ solomon-mybatis/src/main/java/com/steven/solomon/config/MyBatisTenantsContext.java
@@ -0,0 +1,63 @@
+package com.steven.solomon.config;
+
+import com.steven.solomon.context.TenantContext;
+import com.steven.solomon.properties.TenantDataSourceProperties;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.apache.ibatis.datasource.pooled.PooledDataSourceFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyBatisTenantsContext extends TenantContext<PooledDataSourceFactory, TenantDataSourceProperties> {
+
+  private  ThreadLocal<PooledDataSourceFactory> MYBATIS_DB_FACTORY_THREAD_LOCAL = new ThreadLocal<>();
+
+  private  Map<String,PooledDataSourceFactory> MYBATIS_FACTORY_MAP = new ConcurrentHashMap<>();
+
+  private Map<String,TenantDataSourceProperties> mybatisClientMap = new ConcurrentHashMap<>();
+
+  @Override
+  public PooledDataSourceFactory getFactory() {
+    return MYBATIS_DB_FACTORY_THREAD_LOCAL.get();
+  }
+
+  @Override
+  public void setFactory(String key) {
+    MYBATIS_DB_FACTORY_THREAD_LOCAL.set(MYBATIS_FACTORY_MAP.get(key));
+  }
+
+  @Override
+  public void removeFactory() {
+    MYBATIS_DB_FACTORY_THREAD_LOCAL.remove();
+  }
+
+  @Override
+  public Map<String, PooledDataSourceFactory> getFactoryMap() {
+    return MYBATIS_FACTORY_MAP;
+  }
+
+  @Override
+  public void setFactory(Map<String, PooledDataSourceFactory> factoryMap) {
+    MYBATIS_FACTORY_MAP.putAll(factoryMap);
+  }
+
+  @Override
+  public void setFactory(String key, PooledDataSourceFactory factory) {
+    MYBATIS_FACTORY_MAP.put(key, factory);
+  }
+
+  @Override
+  public Map<String, TenantDataSourceProperties> getPropertiesMap() {
+    return mybatisClientMap;
+  }
+
+  @Override
+  public void setProperties(String key, TenantDataSourceProperties properties) {
+    mybatisClientMap.put(key, properties);
+  }
+
+  @Override
+  public void setProperties(Map<String, TenantDataSourceProperties> propertiesMap) {
+    mybatisClientMap.putAll(propertiesMap);
+  }
+}
---
+++ solomon-mybatis/src/main/java/com/steven/solomon/config/MybatisConfig.java
@@ -0,0 +1,11 @@
+package com.steven.solomon.config;
+
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(2)
+@DependsOn({"springUtil"})
+public class MybatisConfig {
+}
