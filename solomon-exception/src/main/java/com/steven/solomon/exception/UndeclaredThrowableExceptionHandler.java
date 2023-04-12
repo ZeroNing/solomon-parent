0a6d508dc46ff3acb8a31816ccf81331da540617
huangweihua <744302215@qq.com>
2023-04-12 15:03:41 +0800

    新增异常捕获器

---
+++ solomon-exception/src/main/java/com/steven/solomon/exception/UndeclaredThrowableExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.steven.solomon.exception;
+
+import com.steven.solomon.constant.code.BaseExceptionCode;
+import com.steven.solomon.vo.BaseExceptionVO;
+import java.lang.reflect.UndeclaredThrowableException;
+import org.springframework.stereotype.Component;
+
+@Component("UndeclaredThrowableExceptionProcessor")
+public class UndeclaredThrowableExceptionHandler extends AbstractExceptionHandler {
+
+  @Override
+  public BaseExceptionVO handleBaseException(Exception ex) {
+    UndeclaredThrowableException e = (UndeclaredThrowableException) ex;
+    Throwable t = ((UndeclaredThrowableException) ex).getUndeclaredThrowable();
+    if(t instanceof BaseException){
+      BaseException baseException = (BaseException) t;
+      return new BaseExceptionVO(baseException.getCode(),baseException.getMessage(),500);
+    }
+    return new BaseExceptionVO(BaseExceptionCode.BASE_EXCEPTION_CODE,500);
+  }
+}
