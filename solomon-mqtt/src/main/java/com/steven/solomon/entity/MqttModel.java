34b803ff03fab194f8113348230b3c05b9b56378
ZeroNing <744302215@qq.com>
2023-04-19 20:57:10 +0800

    调整代码结构

--- solomon-mqtt/src/main/java/com/steven/solomon/entity/MqttModel.java
+++ solomon-mqtt/src/main/java/com/steven/solomon/entity/MqttModel.java
@@ -1,30 +1,18 @@
 package com.steven.solomon.entity;
 
+import com.steven.solomon.pojo.BaseMq;
+
 /**
  * 基础发送MQ基类
  */
-public class MqttModel<T>  {
-
-  /**
-   * 消费者数据
-   */
-  private              T body;
+public class MqttModel<T> extends BaseMq<T> {
 
   public MqttModel() {
     super();
   }
 
   public MqttModel(T body) {
-    super();
-    this.body = body;
-  }
-
-  public T getBody() {
-    return body;
-  }
-
-  public void setBody(T body) {
-    this.body = body;
+    super(body);
   }
 
 }
