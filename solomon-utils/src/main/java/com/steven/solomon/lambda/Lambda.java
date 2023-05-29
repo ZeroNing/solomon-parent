bd8b0bd04352770c33a2765464ab98de3e74ef56
huangweihua <744302215@qq.com>
2023-05-29 09:33:26 +0800

    调整lambda部分方法支持并行流处理

--- solomon-utils/src/main/java/com/steven/solomon/lambda/Lambda.java
+++ solomon-utils/src/main/java/com/steven/solomon/lambda/Lambda.java
@@ -10,6 +10,7 @@ import java.util.function.ToDoubleFunction;
 import java.util.function.ToIntFunction;
 import java.util.function.ToLongFunction;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Lambda {
 
@@ -20,11 +21,25 @@ public class Lambda {
    * @param predicate 条件筛选数据
    * @param func      需要的字段
    */
-  public static <T, S> List<T> toList(Collection<S> list, Predicate<S> predicate, Function<S, T> func) {
+  public static <T, S> List<T> toList(Collection<S> list, Predicate<S> predicate, Function<S, T> func,boolean isParallelStream) {
     if (ValidateUtils.isEmpty(list)) {
       return new ArrayList<>();
     }
-    return list.stream().filter(predicate).map(func).collect(Collectors.toList());
+    Stream<S> stream = isParallelStream ? list.parallelStream() : list.stream();
+    if(ValidateUtils.isNotEmpty(predicate)){
+      stream = stream.filter(predicate);
+    }
+    return stream.map(func).collect(Collectors.toList());
+  }
+
+  /**
+   * 转list
+   *
+   * @param list 数据集合
+   * @param func 需要的字段
+   */
+  public static <T, S> List<T> toList(Collection<S> list, Function<S, T> func,boolean isParallelStream) {
+    return toList(list,null,func,isParallelStream);
   }
 
   /**
@@ -34,10 +49,7 @@ public class Lambda {
    * @param func 需要的字段
    */
   public static <T, S> List<T> toList(Collection<S> list, Function<S, T> func) {
-    if (ValidateUtils.isEmpty(list)) {
-      return new ArrayList<>();
-    }
-    return list.stream().map(func).collect(Collectors.toList());
+    return toList(list,null,func,false);
   }
 
   /**
@@ -47,11 +59,25 @@ public class Lambda {
    * @param predicate 条件筛选数据
    * @param func      需要的字段
    */
-  public static <T, S> Set<T> toSet(Collection<S> list, Predicate<S> predicate, Function<S, T> func) {
+  public static <T, S> Set<T> toSet(Collection<S> list, Predicate<S> predicate, Function<S, T> func,boolean isParallelStream) {
     if (ValidateUtils.isEmpty(list)) {
       return new HashSet<>();
     }
-    return list.stream().filter(predicate).map(func).collect(Collectors.toSet());
+    Stream<S> stream = isParallelStream ? list.parallelStream() : list.stream();
+    if(ValidateUtils.isNotEmpty(predicate)){
+      stream = stream.filter(predicate);
+    }
+    return stream.map(func).collect(Collectors.toSet());
+  }
+
+  /**
+   * 转set
+   *
+   * @param list 数据集合
+   * @param func 需要的字段
+   */
+  public static <T, S> Set<T> toSet(Collection<S> list, Function<S, T> func,boolean isParallelStream) {
+    return toSet(list,null,func,isParallelStream);
   }
 
   /**
@@ -61,10 +87,21 @@ public class Lambda {
    * @param func 需要的字段
    */
   public static <T, S> Set<T> toSet(Collection<S> list, Function<S, T> func) {
+    return toSet(list,null,func,false);
+  }
+
+  /**
+   * 转map
+   *
+   * @param list    数据集合
+   * @param keyFunc 需要的字段
+   */
+  public static <K, T> Map<K, T> toMap(Collection<T> list, Function<T, K> keyFunc,boolean isParallelStream) {
     if (ValidateUtils.isEmpty(list)) {
-      return new HashSet<>();
+      return new HashMap<>();
     }
-    return list.stream().map(func).collect(Collectors.toSet());
+    Stream<T> stream = isParallelStream ? list.parallelStream() : list.stream();
+    return stream.collect(Collectors.toMap(keyFunc, Function.identity(), (key1, key2) -> key2));
   }
 
   /**
@@ -74,10 +111,7 @@ public class Lambda {
    * @param keyFunc 需要的字段
    */
   public static <K, T> Map<K, T> toMap(Collection<T> list, Function<T, K> keyFunc) {
-    if (ValidateUtils.isEmpty(list)) {
-      return new HashMap<>();
-    }
-    return list.stream().collect(Collectors.toMap(keyFunc, Function.identity(), (key1, key2) -> key2));
+    return toMap(list,keyFunc,false);
   }
 
   /**
@@ -88,11 +122,45 @@ public class Lambda {
    * @param keyFunc   需要的字段
    */
   public static <K, T> Map<K, T> toMapPredicate(Collection<T> list, Predicate<T> predicate, Function<T, K> keyFunc) {
+    return toMapPredicate(list,predicate,keyFunc,false);
+  }
+
+  /**
+   * 转map
+   *
+   * @param list      数据集合
+   * @param predicate 条件筛选数据
+   * @param keyFunc   需要的字段
+   */
+  public static <K, T> Map<K, T> toMapPredicate(Collection<T> list, Predicate<T> predicate, Function<T, K> keyFunc,boolean isParallelStream) {
     if (ValidateUtils.isEmpty(list)) {
       return new HashMap<>();
     }
-    return list.stream().filter(predicate)
-        .collect(Collectors.toMap(keyFunc, Function.identity(), (key1, key2) -> key2));
+    Stream<T> stream = isParallelStream ? list.parallelStream() : list.stream();
+    if(ValidateUtils.isNotEmpty(predicate)){
+      stream = stream.filter(predicate);
+    }
+    return stream.collect(Collectors.toMap(keyFunc, Function.identity(), (key1, key2) -> key2));
+  }
+
+  /**
+   * 转map
+   *
+   * @param list      数据集合
+   * @param predicate 条件筛选数据
+   * @param keyFunc   需要的字段
+   * @param valFunc   需要的字段
+   */
+  public static <K, V, T> Map<K, V> toMapPredicate(Collection<T> list, Predicate<T> predicate, Function<T, K> keyFunc,
+      Function<T, V> valFunc,boolean isParallelStream) {
+    if (ValidateUtils.isEmpty(list)) {
+      return new HashMap<>();
+    }
+    Stream<T> stream = isParallelStream ? list.parallelStream() : list.stream();
+    if(ValidateUtils.isNotEmpty(predicate)){
+      stream = stream.filter(predicate);
+    }
+    return stream.collect(Collectors.toMap(keyFunc, valFunc, (key1, key2) -> key2));
   }
 
   /**
@@ -105,10 +173,22 @@ public class Lambda {
    */
   public static <K, V, T> Map<K, V> toMapPredicate(Collection<T> list, Predicate<T> predicate, Function<T, K> keyFunc,
       Function<T, V> valFunc) {
+    return toMapPredicate(list,predicate,keyFunc,valFunc,false);
+  }
+
+  /**
+   * 转map
+   *
+   * @param list    数据集合
+   * @param keyFunc 需要的字段
+   * @param valFunc 需要的字段
+   */
+  public static <K, V, T> Map<K, V> toMap(Collection<T> list, Function<T, K> keyFunc, Function<T, V> valFunc,boolean isParallelStream) {
     if (ValidateUtils.isEmpty(list)) {
       return new HashMap<>();
     }
-    return list.stream().filter(predicate).collect(Collectors.toMap(keyFunc, valFunc, (key1, key2) -> key2));
+    Stream<T> stream = isParallelStream ? list.parallelStream() : list.stream();
+    return stream.collect(Collectors.toMap(keyFunc, valFunc, (key1, key2) -> key2));
   }
 
   /**
@@ -119,10 +199,25 @@ public class Lambda {
    * @param valFunc 需要的字段
    */
   public static <K, V, T> Map<K, V> toMap(Collection<T> list, Function<T, K> keyFunc, Function<T, V> valFunc) {
+    return toMap(list,keyFunc,valFunc,false);
+  }
+
+  /**
+   * 分组
+   *
+   * @param list        数据集合
+   * @param predicate   条件筛选数据
+   * @param groupByFunc 分组需要字段
+   */
+  public static <K, T> Map<K, List<T>> groupBy(Collection<T> list, Predicate<T> predicate, Function<T, K> groupByFunc,boolean isParallelStream) {
     if (ValidateUtils.isEmpty(list)) {
       return new HashMap<>();
     }
-    return list.stream().collect(Collectors.toMap(keyFunc, valFunc, (key1, key2) -> key2));
+    Stream<T> stream = isParallelStream ? list.parallelStream() : list.stream();
+    if(ValidateUtils.isNotEmpty(predicate)){
+      stream = stream.filter(predicate);
+    }
+    return stream.collect(Collectors.groupingBy(groupByFunc));
   }
 
   /**
@@ -133,10 +228,7 @@ public class Lambda {
    * @param groupByFunc 分组需要字段
    */
   public static <K, T> Map<K, List<T>> groupBy(Collection<T> list, Predicate<T> predicate, Function<T, K> groupByFunc) {
-    if (ValidateUtils.isEmpty(list)) {
-      return new HashMap<>();
-    }
-    return list.stream().filter(predicate).collect(Collectors.groupingBy(groupByFunc));
+    return groupBy(list,predicate,groupByFunc,false);
   }
 
   /**
@@ -149,7 +241,7 @@ public class Lambda {
     if (ValidateUtils.isEmpty(list)) {
       return new HashMap<>();
     }
-    return list.stream().collect(Collectors.groupingBy(groupByFunc));
+    return groupBy(list,null,groupByFunc,false);
   }
 
   /**
@@ -159,7 +251,32 @@ public class Lambda {
    *             ram func 分组汇总相加的字段
    */
   public static <T> Integer sum(Collection<T> list, ToIntFunction<T> func) {
-    return list.stream().mapToInt(func).sum();
+    return sum(list,null,func,false);
+  }
+
+  /**
+   * 汇总
+   *
+   * @param list 数据集合
+   *             ram func 分组汇总相加的字段
+   */
+  public static <T> Integer sum(Collection<T> list, ToIntFunction<T> func,boolean isParallelStream) {
+    return sum(list,null,func,isParallelStream);
+  }
+
+  /**
+   * 汇总
+   *
+   * @param list      数据集合
+   * @param predicate 条件筛选数据
+   * @param func      分组汇总相加的字段
+   */
+  public static <T> Integer sum(Collection<T> list, Predicate<T> predicate, ToIntFunction<T> func,boolean isParallelStream) {
+    Stream<T> stream = isParallelStream ? list.parallelStream() : list.stream();
+    if(ValidateUtils.isNotEmpty(predicate)){
+      stream = stream.filter(predicate);
+    }
+    return stream.mapToInt(func).sum();
   }
 
   /**
@@ -170,7 +287,7 @@ public class Lambda {
    * @param func      分组汇总相加的字段
    */
   public static <T> Integer sum(Collection<T> list, Predicate<T> predicate, ToIntFunction<T> func) {
-    return list.stream().filter(predicate).mapToInt(func).sum();
+    return sum(list,predicate,func,false);
   }
 
   /**
@@ -180,7 +297,32 @@ public class Lambda {
    * @param func 分组汇总相加的字段
    */
   public static <T> Long sum(Collection<T> list, ToLongFunction<T> func) {
-    return list.stream().mapToLong(func).sum();
+    return sum(list,null,func,false);
+  }
+
+  /**
+   * 汇总
+   *
+   * @param list 数据集合
+   * @param func 分组汇总相加的字段
+   */
+  public static <T> Long sum(Collection<T> list, ToLongFunction<T> func,boolean isParallelStream) {
+    return sum(list,null,func,isParallelStream);
+  }
+
+  /**
+   * 汇总
+   *
+   * @param list      数据集合
+   * @param predicate 条件筛选数据
+   * @param func      分组汇总相加的字段
+   */
+  public static <T> Long sum(Collection<T> list, Predicate<T> predicate, ToLongFunction<T> func,boolean isParallelStream) {
+    Stream<T> stream = isParallelStream ? list.parallelStream() : list.stream();
+    if(ValidateUtils.isNotEmpty(predicate)){
+      stream = stream.filter(predicate);
+    }
+    return stream.mapToLong(func).sum();
   }
 
   /**
@@ -191,7 +333,7 @@ public class Lambda {
    * @param func      分组汇总相加的字段
    */
   public static <T> Long sum(Collection<T> list, Predicate<T> predicate, ToLongFunction<T> func) {
-    return list.stream().filter(predicate).mapToLong(func).sum();
+    return sum(list,predicate,func,false);
   }
 
   /**
@@ -201,7 +343,32 @@ public class Lambda {
    * @param func 分组汇总相加的字段
    */
   public static <T, K> Double sum(Collection<T> list, ToDoubleFunction<T> func) {
-    return list.stream().mapToDouble(func).sum();
+    return sum(list,func,false);
+  }
+
+  /**
+   * 汇总
+   *
+   * @param list 数据集合
+   * @param func 分组汇总相加的字段
+   */
+  public static <T, K> Double sum(Collection<T> list, ToDoubleFunction<T> func,boolean isParallelStream) {
+    return sum(list,func,isParallelStream);
+  }
+
+  /**
+   * 汇总
+   *
+   * @param list      数据集合
+   * @param predicate 条件筛选数据
+   * @param func      分组汇总相加的字段
+   */
+  public static <T> Double sum(Collection<T> list, Predicate<T> predicate, ToDoubleFunction<T> func,boolean isParallelStream) {
+    Stream<T> stream = isParallelStream ? list.parallelStream() : list.stream();
+    if(ValidateUtils.isNotEmpty(predicate)){
+      stream = stream.filter(predicate);
+    }
+    return stream.mapToDouble(func).sum();
   }
 
   /**
@@ -212,7 +379,7 @@ public class Lambda {
    * @param func      分组汇总相加的字段
    */
   public static <T> Double sum(Collection<T> list, Predicate<T> predicate, ToDoubleFunction<T> func) {
-    return list.stream().filter(predicate).mapToDouble(func).sum();
+    return sum(list,predicate,func,false);
   }
 
   /**
