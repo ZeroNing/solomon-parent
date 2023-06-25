d0228cc25e6857d7b71c736e58c3c17752d65927
huangweihua <744302215@qq.com>
2023-06-25 14:45:07 +0800

    调整语言编码以及json转换器

---
+++ solomon-common/src/main/java/com/steven/solomon/config/WebConfig.java
@@ -0,0 +1,36 @@
+package com.steven.solomon.config;
+
+import com.steven.solomon.json.config.JacksonObjectMapper;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration(proxyBeanMethods = false)
+@Import(value = {JacksonObjectMapper.class})
+public class WebConfig extends WebMvcConfigurationSupport {
+
+  @Autowired
+  private JacksonObjectMapper jacksonObjectMapper;
+
+  @Override
+  protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+    converters.stream()
+        // 过滤出StringHttpMessageConverter类型实例
+        .filter(StringHttpMessageConverter.class::isInstance)
+        .map(c -> (StringHttpMessageConverter) c)
+        // 这里将转换器的默认编码设置为utf-8
+        .forEach(c -> c.setDefaultCharset(StandardCharsets.UTF_8));
+    //创建消息转换器对象
+    MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+    //设置对象转换器，底层使用Jackson将Java对象转为json
+    messageConverter.setObjectMapper(jacksonObjectMapper);
+    //将上面的消息转换器对象追加到mvc框架的转换器集合中
+    converters.add(0,messageConverter);
+  }
+}
