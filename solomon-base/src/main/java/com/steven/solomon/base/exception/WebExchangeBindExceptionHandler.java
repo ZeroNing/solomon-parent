b40d80308ff5eb01116fee0d285328e5b265e3cd
huangweihua <744302215@qq.com>
2023-07-13 17:49:55 +0800

    新增一种异常捕获处理器

---
+++ solomon-base/src/main/java/com/steven/solomon/base/exception/WebExchangeBindExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.steven.solomon.base.exception;
+
+import com.steven.solomon.exception.handler.AbstractExceptionHandler;
+import com.steven.solomon.pojo.vo.BaseExceptionVO;
+import com.steven.solomon.verification.ValidateUtils;
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.support.WebExchangeBindException;
+
+@Configuration(value = "WebExchangeBindExceptionProcessor",proxyBeanMethods = false)
+public class WebExchangeBindExceptionHandler extends AbstractExceptionHandler {
+
+  @Override
+  public BaseExceptionVO handleBaseException(Exception ex) {
+    List<ObjectError> errorMessages = ((WebExchangeBindException) ex).getAllErrors();
+    String            errorMessage  = null;
+    if (ValidateUtils.isNotEmpty(errorMessages)) {
+      ObjectError objectError = errorMessages.get(0);
+      errorMessage = objectError.getDefaultMessage();
+    }
+    return new BaseExceptionVO(errorMessage, 500);
+  }
+}
