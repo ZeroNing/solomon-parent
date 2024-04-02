33ef32108bdccf1493dd8ca4f7d12f3457049fea
huangweihua <744302215@qq.com>
2024-04-02 15:14:41 +0800

    新增mqtt多租户初始方法

--- solomon-mqtt/src/main/java/com/steven/solomon/config/MqttConfig.java
+++ solomon-mqtt/src/main/java/com/steven/solomon/config/MqttConfig.java
@@ -56,30 +56,6 @@ public class MqttConfig {
     SpringUtil.setContext(applicationContext);
   }
 
-  public MqttConnectOptions getMqttConnectOptions(MqttProfile mqttProfile) {
-    MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
-    mqttConnectOptions.setUserName(mqttProfile.getUserName());
-    mqttConnectOptions.setPassword(mqttProfile.getPassword().toCharArray());
-    mqttConnectOptions.setServerURIs(new String[]{mqttProfile.getUrl()});
-    //设置同一时间可以发送的最大未确认消息数量
-    mqttConnectOptions.setMaxInflight(mqttProfile.getMaxInflight());
-    //设置超时时间
-    mqttConnectOptions.setConnectionTimeout(mqttProfile.getCompletionTimeout());
-    //设置自动重连
-    mqttConnectOptions.setAutomaticReconnect(mqttProfile.getAutomaticReconnect());
-    //cleanSession 设为 true;当客户端掉线时;服务器端会清除 客户端session;重连后 客户端会有一个新的session,cleanSession
-    // 设为false，客户端掉线后 服务器端不会清除session，当重连后可以接收之前订阅主题的消息。当客户端上线后会接受到它离线的这段时间的消息
-    mqttConnectOptions.setCleanSession(mqttProfile.getCleanSession());
-    // 设置会话心跳时间 单位为秒   设置会话心跳时间 单位为秒 服务器会每隔1.5*20秒的时间向客户端发送心跳判断客户端是否在线，但这个方法并没有重连的机制
-    mqttConnectOptions.setKeepAliveInterval(mqttProfile.getKeepAliveInterval());
-    //设置遗嘱消息
-    if (ValidateUtils.isNotEmpty(mqttProfile.getWill())) {
-      MqttWill will = mqttProfile.getWill();
-      mqttConnectOptions.setWill(will.getTopic(), will.getMessage().getBytes(), will.getQos(), will.getRetained());
-    }
-    return mqttConnectOptions;
-  }
-
   @PostConstruct
   public void afterPropertiesSet() throws MqttException {
     Map<String,MqttProfile> tenantProfileMap = profile.getTenant();
@@ -87,36 +63,7 @@ public class MqttConfig {
       return ;
     }
     for(Entry<String,MqttProfile> entry: tenantProfileMap.entrySet()){
-      String tenantCode = entry.getKey();
-      MqttProfile mqttProfile = entry.getValue();
-
-      MqttClient   mqttClient = new MqttClient(mqttProfile.getUrl(), ValidateUtils.getOrDefault(mqttProfile.getClientId(),UUID.randomUUID().toString()));
-      List<Object> clazzList  = new ArrayList<>(SpringUtil.getBeansWithAnnotation(Mqtt.class).values());
-      MqttConnectOptions options = getMqttConnectOptions(mqttProfile);
-      mqttClient.connect(options);
-      utils.putOptionsMap(tenantCode,options);
-
-      if (ValidateUtils.isNotEmpty(clazzList)) {
-        Map<AbstractConsumer, Mqtt> map = new HashMap<>(clazzList.size());
-        for (Object abstractConsumer : clazzList) {
-          Mqtt mqtt = AnnotationUtils.findAnnotation(abstractConsumer.getClass(), Mqtt.class);
-
-          if (ValidateUtils.isEmpty(mqtt)) {
-            continue;
-          }
-          AbstractConsumer consumer = (AbstractConsumer) BeanUtil.copyProperties(abstractConsumer,abstractConsumer.getClass(), (String) null);
-          for(String topic : mqtt.topics()){
-            mqttClient.subscribe(topic, mqtt.qos(), consumer);
-          }
-          map.put(consumer, mqtt);
-          utils.putTenantAbstractConsumerMap(tenantCode,map);
-        }
-      }
-      Collection<MqttCallback> mqttCallbacks = SpringUtil.getBeansOfType(MqttCallback.class).values();
-      if (ValidateUtils.isNotEmpty(mqttCallbacks)) {
-        mqttClient.setCallback(mqttCallbacks.stream().findFirst().get());
-      }
-      utils.putClient(tenantCode,mqttClient);
+      utils.init(entry.getKey(),entry.getValue());
     }
   }
 
--- solomon-mqtt/src/main/java/com/steven/solomon/utils/MqttUtils.java
+++ solomon-mqtt/src/main/java/com/steven/solomon/utils/MqttUtils.java
@@ -1,25 +1,35 @@
 package com.steven.solomon.utils;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.lang.UUID;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.steven.solomon.annotation.Mqtt;
 import com.steven.solomon.consumer.AbstractConsumer;
 import com.steven.solomon.entity.MqttModel;
+import com.steven.solomon.profile.MqttProfile;
+import com.steven.solomon.profile.MqttProfile.MqttWill;
+import com.steven.solomon.spring.SpringUtil;
 import com.steven.solomon.utils.logger.LoggerUtils;
 import com.steven.solomon.service.SendService;
 import com.steven.solomon.verification.ValidateUtils;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import javax.annotation.Resource;
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.slf4j.Logger;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
 
 @Configuration
@@ -55,6 +65,37 @@ public class MqttUtils implements SendService<MqttModel> {
 
   public void putTenantAbstractConsumerMap(String tenantCode,Map<AbstractConsumer, Mqtt> tenantAbstractConsumerMap) {
     this.tenantAbstractConsumerMap.put(tenantCode, tenantAbstractConsumerMap);
+  }
+
+  public void init(String tenantCode, MqttProfile mqttProfile) throws MqttException {
+    MqttClient mqttClient = new MqttClient(mqttProfile.getUrl(), ValidateUtils.getOrDefault(mqttProfile.getClientId(), UUID.randomUUID().toString()));
+    List<Object>       clazzList = new ArrayList<>(SpringUtil.getBeansWithAnnotation(Mqtt.class).values());
+    MqttConnectOptions options   = getMqttConnectOptions(mqttProfile);
+    mqttClient.connect(options);
+    putOptionsMap(tenantCode,options);
+
+    if (ValidateUtils.isNotEmpty(clazzList)) {
+      Map<AbstractConsumer, Mqtt> map = new HashMap<>(clazzList.size());
+      for (Object abstractConsumer : clazzList) {
+        Mqtt mqtt = AnnotationUtils.findAnnotation(abstractConsumer.getClass(), Mqtt.class);
+
+        if (ValidateUtils.isEmpty(mqtt)) {
+          continue;
+        }
+        AbstractConsumer consumer = (AbstractConsumer) BeanUtil
+            .copyProperties(abstractConsumer,abstractConsumer.getClass(), (String) null);
+        for(String topic : mqtt.topics()){
+          mqttClient.subscribe(topic, mqtt.qos(), consumer);
+        }
+        map.put(consumer, mqtt);
+        putTenantAbstractConsumerMap(tenantCode,map);
+      }
+    }
+    Collection<MqttCallback> mqttCallbacks = SpringUtil.getBeansOfType(MqttCallback.class).values();
+    if (ValidateUtils.isNotEmpty(mqttCallbacks)) {
+      mqttClient.setCallback(mqttCallbacks.stream().findFirst().get());
+    }
+    putClient(tenantCode,mqttClient);
   }
 
   /**
@@ -134,5 +175,29 @@ public class MqttUtils implements SendService<MqttModel> {
         }
       }
     }
+  }
+
+  private MqttConnectOptions getMqttConnectOptions(MqttProfile mqttProfile) {
+    MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+    mqttConnectOptions.setUserName(mqttProfile.getUserName());
+    mqttConnectOptions.setPassword(mqttProfile.getPassword().toCharArray());
+    mqttConnectOptions.setServerURIs(new String[]{mqttProfile.getUrl()});
+    //设置同一时间可以发送的最大未确认消息数量
+    mqttConnectOptions.setMaxInflight(mqttProfile.getMaxInflight());
+    //设置超时时间
+    mqttConnectOptions.setConnectionTimeout(mqttProfile.getCompletionTimeout());
+    //设置自动重连
+    mqttConnectOptions.setAutomaticReconnect(mqttProfile.getAutomaticReconnect());
+    //cleanSession 设为 true;当客户端掉线时;服务器端会清除 客户端session;重连后 客户端会有一个新的session,cleanSession
+    // 设为false，客户端掉线后 服务器端不会清除session，当重连后可以接收之前订阅主题的消息。当客户端上线后会接受到它离线的这段时间的消息
+    mqttConnectOptions.setCleanSession(mqttProfile.getCleanSession());
+    // 设置会话心跳时间 单位为秒   设置会话心跳时间 单位为秒 服务器会每隔1.5*20秒的时间向客户端发送心跳判断客户端是否在线，但这个方法并没有重连的机制
+    mqttConnectOptions.setKeepAliveInterval(mqttProfile.getKeepAliveInterval());
+    //设置遗嘱消息
+    if (ValidateUtils.isNotEmpty(mqttProfile.getWill())) {
+      MqttWill will = mqttProfile.getWill();
+      mqttConnectOptions.setWill(will.getTopic(), will.getMessage().getBytes(), will.getQos(), will.getRetained());
+    }
+    return mqttConnectOptions;
   }
 }
