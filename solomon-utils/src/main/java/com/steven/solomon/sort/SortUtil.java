22df0d4e6a7431364ee967249b0b9735798b7958
ZeroNing <744302215@qq.com>
2024-05-24 15:21:18 +0800

    删除计数排序

--- solomon-utils/src/main/java/com/steven/solomon/sort/SortUtil.java
+++ solomon-utils/src/main/java/com/steven/solomon/sort/SortUtil.java
@@ -9,8 +9,6 @@ public class SortUtil {
 
   private static BucketSortService bucketSortService = new BucketSortService();
 
-  private static CountingSortService countingSortService = new CountingSortService();
-
   private static HeapSortService heapSortService = new HeapSortService();
 
   private static InsertionSortService insertionSortService = new InsertionSortService();
@@ -47,19 +45,6 @@ public class SortUtil {
    */
   public static <T> List<T> bucketSort(List<T> list, Comparator<? super T> comparator, boolean ascending){
     return bucketSortService.sort(list,comparator,ascending);
-  }
-
-  /**
-   * 对给定的列表进行计数排序，支持升序和降序排序。
-   *
-   * @param <T>          列表元素的类型
-   * @param list         要排序的列表
-   * @param comparator   用于比较列表元素的比较器
-   * @param ascending    如果为 true，则进行升序排序；如果为 false，则进行降序排序
-   * @return 排序后的列表
-   */
-  public static <T> List<T> countingSort(List<T> list, Comparator<? super T> comparator, boolean ascending){
-    return countingSortService.sort(list,comparator,ascending);
   }
 
   /**
