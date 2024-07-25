eebd4e4c185d63798d5f8a097ec25f0978a79075
ZeroNing <744302215@qq.com>
2024-07-25 11:16:24 +0800

    调整redis队列部分方法入参

--- solomon-redis/src/main/java/com/steven/solomon/consumer/AbstractConsumer.java
+++ solomon-redis/src/main/java/com/steven/solomon/consumer/AbstractConsumer.java
@@ -20,40 +20,36 @@ public abstract class AbstractConsumer<T,R> extends MessageListenerAdapter {
     @Override
     public void onMessage(Message message, @Nullable byte[] pattern) {
         String body = new String(message.getBody());
-        String channel = new String(message.getChannel());
+        String topic = new String(message.getChannel());
         try {
-          logger.debug("线程名:{},AbstractConsumer:主题:{},消费者消息: {}", Thread.currentThread().getName(),channel, body);
+          logger.debug("线程名:{},AbstractConsumer:主题:{},消费者消息: {}", Thread.currentThread().getName(),topic, body);
           RedisQueueModel model = JackJsonUtils.conversionClass(body, RedisQueueModel.class);
           RequestHeaderHolder.setTenantCode(model.getTenantCode());
-          R result = this.handleMessage((T) model.getBody());
+          R result = this.handleMessage((T) model.getBody(),topic);
           // 保存消费成功消息
-          saveLog(result, message, model);
+          saveLog(result, topic, model);
         } catch (Throwable e){
           // 消费失败次数不等于空并且失败次数大于某个次数,不处理直接return,并记录到数据库
           logger.error("AbstractConsumer:消费报错 异常为:", e);
           // 将消费失败的记录保存到数据库或者不处理也可以
-          this.saveFailMessage(message, e);
+          this.saveFailMessage(body,topic, e);
         }
     }
 
     /**
      * 消费方法
-     * @param body 请求数据
      */
-    public abstract R handleMessage(T body) throws Exception;
+    public abstract R handleMessage(T body,String topic) throws Exception;
 
     /**
      * 保存消费失败的消息
-     *
-     * @param message mq所包含的信息
-     * @param e       异常
      */
-    public void saveFailMessage(Message message, Throwable e) {
+    public void saveFailMessage(String body,String topic, Throwable e) {
 
     }
 
     /**
      * 保存消费成功消息
      */
-    public abstract void saveLog(R result, Message message, RedisQueueModel model);
+    public abstract void saveLog(R result, String topic, RedisQueueModel model);
 }
