1baa068ed6bbbf313d3097653dca454473d1b60d
ZeroNing <744302215@qq.com>
2024-09-29 14:44:51 +0800

    删除无用的redis方法

--- solomon-redis/src/main/java/com/steven/solomon/service/impl/DefaultService.java
+++ solomon-redis/src/main/java/com/steven/solomon/service/impl/DefaultService.java
@@ -14,8 +14,8 @@ public class DefaultService extends AbsICacheService {
   }
 
   @Override
-  public long getExpire(String group, String key) {
-    return 0;
+  public Long getExpire(String group, String key) {
+    return null;
   }
 
   @Override
@@ -30,126 +30,6 @@ public class DefaultService extends AbsICacheService {
 
   @Override
   public <T> T get(String group, String key) {
-    return null;
-  }
-
-  @Override
-  public <T> T set(String group, String key, T value) {
-    return null;
-  }
-
-  @Override
-  public <T> T set(String group, String key, T value, int time) {
-    return null;
-  }
-
-  @Override
-  public <T extends Set> T setGet(String group, String key) {
-    return null;
-  }
-
-  @Override
-  public <T extends Set> T set(String group, String key, int time, T... value) {
-    return null;
-  }
-
-  @Override
-  public <T extends Set> T set(String group, String key, T... value) {
-    return null;
-  }
-
-  @Override
-  public Long remove(String group, String key, Object... value) {
-    return null;
-  }
-
-  @Override
-  public <T> T ListGet(String group, String key, int start, int end) {
-    return null;
-  }
-
-  @Override
-  public <T> T leftPush(String group, String key, T value, int time) {
-    return null;
-  }
-
-  @Override
-  public <T> T leftPush(String group, String key, T value) {
-    return null;
-  }
-
-  @Override
-  public <T> T leftPushAll(String group, String key, int time, T... value) {
-    return null;
-  }
-
-  @Override
-  public <T> T leftPushAll(String group, String key, int time, List<T> value) {
-    return null;
-  }
-
-  @Override
-  public <T> T leftPushAll(String group, String key, T... value) {
-    return null;
-  }
-
-  @Override
-  public <T> T leftPushAll(String group, String key, List<T> value) {
-    return null;
-  }
-
-  @Override
-  public <T> T rightPush(String group, String key, T value, int time) {
-    return null;
-  }
-
-  @Override
-  public <T> T rightPushAll(String group, String key, int time, T... value) {
-    return null;
-  }
-
-  @Override
-  public <T> T rightPushAll(String group, String key, int time, List<T> value) {
-    return null;
-  }
-
-  @Override
-  public <T> T rightPushAll(String group, String key, T... value) {
-    return null;
-  }
-
-  @Override
-  public <T> T rightPushAll(String group, String key, List<T> value) {
-    return null;
-  }
-
-  @Override
-  public <T> T rightPush(String group, String key, T value) {
-    return null;
-  }
-
-  @Override
-  public <T> T hashGet(String group, String key, String hashKey) {
-    return null;
-  }
-
-  @Override
-  public <T> T put(String group, String key, String hashKey, T value) {
-    return null;
-  }
-
-  @Override
-  public <T> T put(String group, String key, String hashKey, T value, int time) {
-    return null;
-  }
-
-  @Override
-  public <T extends Map> T putAll(String group, String key, T value, int time) {
-    return null;
-  }
-
-  @Override
-  public <T extends Map> T putAll(String group, String key, T value) {
     return null;
   }
 
