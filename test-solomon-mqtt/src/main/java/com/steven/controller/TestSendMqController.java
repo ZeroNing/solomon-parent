eb6b8e0be84c75c8d172ca5b4bb003da7d64cb98
huangweihua <744302215@qq.com>
2024-10-25 13:28:50 +0800

    增加mqtt测试用例

---
+++ test-solomon-mqtt/src/main/java/com/steven/controller/TestSendMqController.java
@@ -0,0 +1,22 @@
+package com.steven.controller;
+
+import com.steven.solomon.entity.MqttModel;
+import com.steven.solomon.utils.MqttUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestSendMqController {
+
+    private final MqttUtils utils;
+
+    public TestSendMqController(MqttUtils utils) {
+        this.utils = utils;
+    }
+
+    @GetMapping("/test")
+    public Object test() throws Exception {
+        utils.send(new MqttModel<String>("test","top/test/123","123"));
+        return null;
+    }
+}
