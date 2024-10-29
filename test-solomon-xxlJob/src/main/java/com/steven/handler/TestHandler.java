3f32e01c400925ff3914c924febf5681a81c67e8
huangweihua <744302215@qq.com>
2024-10-29 15:57:26 +0800

    提交xxl-job的测试代码

---
+++ test-solomon-xxlJob/src/main/java/com/steven/handler/TestHandler.java
@@ -0,0 +1,21 @@
+package com.steven.handler;
+
+import com.steven.solomon.annotation.JobTask;
+import com.steven.solomon.consumer.AbstractJobConsumer;
+import com.steven.solomon.enums.ScheduleTypeEnum;
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import groovyjarjarpicocli.CommandLine;
+
+@JobTask(taskName = "123456", author = "123456", executorHandler = "testHandler",scheduleType = ScheduleTypeEnum.FIX_RATE,scheduleConf = "60")
+public class TestHandler extends AbstractJobConsumer {
+
+    @Override
+    public void handle(String jobParam) {
+    }
+
+    @Override
+    public void saveLog(Throwable throwable) {
+
+    }
+}
