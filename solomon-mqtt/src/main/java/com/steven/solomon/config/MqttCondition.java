a867a4167086b400817e4c2b7d1d0acbc4664c41
huangweihua <744302215@qq.com>
2024-11-01 11:38:37 +0800

    调整开关控制

--- solomon-mqtt/src/main/java/com/steven/solomon/config/MqttCondition.java
+++ solomon-mqtt/src/main/java/com/steven/solomon/config/MqttCondition.java
@@ -1,6 +1,7 @@
 package com.steven.solomon.config;
 
 import cn.hutool.core.util.BooleanUtil;
+import com.steven.solomon.verification.ValidateUtils;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
@@ -8,7 +9,7 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 public class MqttCondition implements Condition {
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        String enabled = context.getEnvironment().getProperty("mqtt.enabled");
+        String enabled = ValidateUtils.getOrDefault(context.getEnvironment().getProperty("mqtt.enabled"),"true");
         return BooleanUtil.toBoolean(enabled);
     }
 }
