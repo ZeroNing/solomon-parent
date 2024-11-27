041ead12f970713f6c1139d7dfdbe1d59f601d20
huangweihua <744302215@qq.com>
2024-11-27 11:24:38 +0800

    优化xxljob方法

--- solomon-xxlJob/src/main/java/com/steven/solomon/consumer/AbstractJobConsumer.java
+++ solomon-xxlJob/src/main/java/com/steven/solomon/consumer/AbstractJobConsumer.java
@@ -15,29 +15,27 @@ public abstract class AbstractJobConsumer extends IJobHandler {
 
     private final JobTask jobTask = getClass().getAnnotation(JobTask.class);
 
+    private final String xxlJobBeanName = ValidateUtils.isNotEmpty(jobTask) ? ValidateUtils.getOrDefault(jobTask.executorHandler(),getClass().getSimpleName()) : getClass().getSimpleName();
+
     public void execute() throws Exception{
         String jobParam = XxlJobHelper.getJobParam();
-        logger.info("BeanName:{},任务参数:{},开启任务调度",getXxlJobBeanName(),jobParam);
+        logger.info("BeanName:{},任务参数:{},开启任务调度",xxlJobBeanName,jobParam);
         StopWatch stopWatch = new StopWatch();
         try {
             stopWatch.start();
             handle(jobParam);
         }catch(Throwable e){
-            logger.error("BeanName:{} AbstractJobConsumer:调度报错 异常为:",getXxlJobBeanName(), e);
+            logger.error("BeanName:{} AbstractJobConsumer:调度报错 异常为:",xxlJobBeanName, e);
             saveLog(e);
             throw e;
         } finally {
             stopWatch.stop();
             Double second = Double.parseDouble(String.valueOf(stopWatch.getLastTaskTimeMillis())) / 1000;
-            logger.info("BeanName:{},结束任务调度,耗时:{}秒",getXxlJobBeanName(),second);
+            logger.info("BeanName:{},结束任务调度,耗时:{}秒",xxlJobBeanName,second);
         }
     }
 
     public abstract void handle(String jobParam) throws Exception;
-
-    public String getXxlJobBeanName(){
-        return ValidateUtils.isNotEmpty(jobTask) ? ValidateUtils.getOrDefault(jobTask.executorHandler(),getClass().getSimpleName()) : getClass().getSimpleName();
-    }
 
     /**
      * 保存消费失败的消息
