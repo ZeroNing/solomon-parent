c4814779b2dd1327995d1cf9bf16b305167a7260
huangweihua <744302215@qq.com>
2024-12-17 13:03:08 +0800

    调整mqtt测试用例 Rabbitmq发送消息支持优先级

--- test-solomon-mqtt/src/main/java/com/steven/handler/TestHandler.java
+++ test-solomon-mqtt/src/main/java/com/steven/handler/TestHandler.java
@@ -7,14 +7,15 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 @MessageListener(topics = "top/+/123",tenantRange = "test12")
 public class TestHandler extends AbstractConsumer<String,String> {
+
     @Override
-    public String handleMessage(String topic, String tenantCode, String body) throws Exception {
+    public String handleMessage(String body) throws Exception {
         logger.info("接受的主题是:{},内容是:{}",topic,body);
         return "";
     }
 
     @Override
-    public void saveLog(String result, MqttMessage message, MqttModel<String> model, Throwable e) {
+    public void saveLog(String result, Throwable throwable, MqttModel<String> model) {
 
     }
 }
