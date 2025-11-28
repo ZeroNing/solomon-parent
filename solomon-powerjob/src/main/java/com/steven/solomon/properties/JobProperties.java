3997eae94b0235f5a665a7b067b1622e5a68237f
steven <744302215@qq.com>
2025-11-28 02:20:25 +0000

    update solomon-powerjob/src/main/java/com/steven/solomon/properties/JobProperties.java.
    Signed-off-by: steven <1858145+solomon-steven@user.noreply.gitee.com>

--- solomon-powerjob/src/main/java/com/steven/solomon/properties/JobProperties.java
+++ solomon-powerjob/src/main/java/com/steven/solomon/properties/JobProperties.java
@@ -11,6 +11,17 @@ public class JobProperties {
 
     private String namespace;
 
+    //是否自动注册
+    private boolean autoRegister = false;
+
+    public boolean getAutoRegister() {
+        return autoRegister;
+    }
+
+    public void setAutoRegister(boolean autoRegister) {
+        this.autoRegister = autoRegister;
+    }
+
     public String getNamespace() {
         return namespace;
     }
