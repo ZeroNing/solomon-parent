244dc157b449dd71d0eeb490326ff5c2c629604c
huangweihua <huangweihua@century-cn.com>
2026-03-24 10:22:07 +0800

    fix() : 新增vertx-mqtt

---
+++ solom-vertx-mqtt/src/main/java/com/steven/solomon/annotation/MessageListener.java
@@ -0,0 +1,36 @@
+package com.steven.solomon.annotation;
+
+import cn.hutool.core.util.StrUtil;
+import com.steven.solomon.config.MqttCondition;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * mqtt注解
+ */
+@Target(value = { ElementType.FIELD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Component
+@Conditional(MqttCondition.class)
+public @interface MessageListener {
+
+  /**
+   * 主题
+   */
+  String[] topics();
+
+  /**
+   * 消息质量
+   */
+  int qos() default 0;
+
+  /**
+   * 允许订阅的租户范围
+   */
+  String[] tenantRange() default StrUtil.EMPTY;
+}
