14ec5ff71e97bcbfb59db41c21e198e8a5c2ffe6
huangweihua <huangweihua@century-cn.com>
2026-05-08 17:07:30 +0800

    refactor(datasource): 移除数据源模块中的注解、配置和转换器相关代码

--- solomon-datasource/src/main/java/com/steven/solomon/config/DataSourceTenantContext.java
+++
@@ -1,8 +0,0 @@
-package com.steven.solomon.config;
-
-import com.steven.solomon.context.TenantContext;
-
-import javax.sql.DataSource;
-
-public class DataSourceTenantContext extends TenantContext<DataSource> {
-}
--- solomon-datasource/src/main/java/com/steven/solomon/convert/ColumnConvert.java
+++
@@ -1,6 +0,0 @@
-package com.steven.solomon.convert;
-
-public interface ColumnConvert<S> {
-
-    S convert(Object value);
-}
